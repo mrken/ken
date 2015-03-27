5a1d93f2d915a9ee1e95cad9413180edccb2042e
ken <849946945@qq.com>
2015-03-27 10:46:54 +0800

    Change Our Web 1.0.0

--- Scripts/Layout.js
+++ Scripts/Layout.js
@@ -8,7 +8,7 @@
         $("#nav .nav-menu a").removeClass("current");
         current.addClass("current");
     });
-    var head = $(".head").height();
+	resize();
     var h = document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight;
     //document.getElementById("main").style.minHeight = h - head - 70 + "px";
     //var height = $(".body").height();
@@ -17,3 +17,15 @@
     //}
     $(".body").css("minHeight", h - head - 102 + "px");
 });
+
+	window.onresize = resize;
+function resize(){
+	var width = $(".main").width();
+	var w = document.documentElement.clientWidth;
+	if(w>width)
+	{
+		var persent = (w-width)/2/w*100;
+		$(".main").css("margin-left",persent+"%");
+	}
+    var head = $(".head").height();
+}
